ee2829be5a00ccc8d43e864abb3aa8077c4aedfe
zohaib <khaliqzohaib92@gmail.com>
2020-08-29 17:37:26 -0400

    Create insert and getAll quries for column

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App/App';
 
-import {insert, getAll, remove, updateTitle, updateColumn, removeColumn} from './database/modules/board/queries'
+import {insert, getAll, remove, updateTitle, addColumn, removeColumn} from './database/modules/board/queries'
+import {insert as columnInsert, getAll as columnGetAll} from './database/modules/column/queries'
 import Board from './models/board';
+import Column from './models/column'
 import Database from './database/database';
-window.board = new Board("This is a test title");
+window.board = new Board("This is a new board title");
+window.column = Column;
 window.boardInsert = insert;
 window.boardGetAll = getAll;
 window.boardRemove = remove;
 window.boardUpdateTitle = updateTitle;
-window.boardupdateColumn = updateColumn;
+window.boardAddColumn = addColumn;
 window.boardRemoveColumn = removeColumn;
+window.columnInsert = columnInsert
+window.columnGetAll = columnGetAll
 window.db = new Database();
 
 ReactDOM.render(
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -6,5 +6,5 @@ export function hashCode (str){
         hash = ((hash<<5)-hash)+char;
         hash = hash & hash; // Convert to 32bit integer
     }
-    return "kk"+(hash < 0 ? -hash : hash).toString();
+    return "kk"+(hash < 0 ? -hash : hash).toString()+new Date().getTime();
 }
