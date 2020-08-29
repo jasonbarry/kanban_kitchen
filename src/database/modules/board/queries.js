e42c851c2bd755b7084158f8c84edcc726aff6ed
zohaib <khaliqzohaib92@gmail.com>
2020-08-29 16:10:41 -0400

    Add remove, updateTitle, updateColumn, and removeColumn to board queries

--- src/database/modules/board/queries.js
+++ src/database/modules/board/queries.js
@@ -1,14 +1,15 @@
-import Database from '../../database';
 import { config } from '../../../setup/DBConfig';
+import Database from '../../database';
 
+const database = new Database();
 
-export const insert = (db, board, onSuccess, onError) => {
-   const res =  db.transaction([config.stores.BOARDS], "readwrite")
+export const insert = (board, onSuccess, onError) => {
+   const res =  database.instance.transaction([config.stores.BOARDS], "readwrite")
     .objectStore(config.stores.BOARDS)
     .add(board);
 
     res.onsuccess = (event) => {
-        onSuccess(event.traget.result);
+        onSuccess(event);
     }
 
     res.onerror = (event) => {
@@ -16,13 +17,103 @@ export const insert = (db, board, onSuccess, onError) => {
     }
 }
 
-export const getAll = (db, onSuccess, onError) => {
-    const res =  db.transaction([config.stores.BOARDS], "readwrite")
+export const getAll = (onSuccess, onError) => {
+    const res =  database.instance.transaction([config.stores.BOARDS], "readwrite")
     .objectStore(config.stores.BOARDS)
     .getAll();
 
     res.onsuccess = (event) => {
-        onSuccess(event.target.result);
+        onSuccess(event.target.result   );
+    }
+
+    res.onerror = (event) => {
+        onError(event);
+    }
+}
+
+export const remove = (id, onSuccess, onError) => {
+    const res =  database.instance.transaction([config.stores.BOARDS], "readwrite")
+    .objectStore(config.stores.BOARDS)
+    .delete(id);
+
+    res.onsuccess = (event) => {
+        onSuccess(event);
+    }
+
+    res.onerror = (event) => {
+        onError(event);
+    }
+}
+
+export const updateTitle = (id, title, onSuccess, onError) => {
+    const store =  database.instance.transaction([config.stores.BOARDS], "readwrite")
+    .objectStore(config.stores.BOARDS)
+
+    const res = store.get(id);
+
+    res.onsuccess = (event) => {
+        const board = event.target.result;
+        board.title = title;
+        update(store, board, onSuccess, onError);
+    }
+
+    res.onerror = (event) => {
+        onError(event);
+    }
+}
+
+const update = (store, board, onSuccess, onError) => {
+
+    const res = store.put(board);
+
+    res.onsuccess = (event) => {
+        if(onSuccess !== undefined)
+            onSuccess(event)
+    }
+
+    res.onerror = (event) => {
+        if(onError !== undefined)
+            onError(event);
+    }
+}
+
+export const updateColumn = (boardId, columnId, position, onSuccess, onError) => {
+    const store =  database.instance.transaction([config.stores.BOARDS], "readwrite")
+    .objectStore(config.stores.BOARDS);
+
+    const res = store.get(boardId);
+
+    res.onsuccess = (event) => {
+        const board = event.target.result;
+
+        if(board.columns === undefined){
+            board.columns = {};
+        }
+
+        board.columns[columnId] = position;
+        update(store, board, onSuccess, onError);
+    }
+
+    res.onerror = (event) => {
+        onError(event);
+    }
+}
+
+export const removeColumn = (boardId, columnId, onSuccess, onError) => {
+    const store =  database.instance.transaction([config.stores.BOARDS], "readwrite")
+    .objectStore(config.stores.BOARDS);
+
+    const res = store.get(boardId);
+
+    res.onsuccess = (event) => {
+        const board = event.target.result;
+
+        if(board.columns !== undefined && board.columns[columnId]){
+            delete board.columns[columnId]
+            update(store, board, onSuccess, onError);
+        } else {
+            onSuccess(event);
+        } 
     }
 
     res.onerror = (event) => {
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App/App';
 
-import Databse from './database/database'
-import {insert, getAll} from './database/modules/board/queries'
+import {insert, getAll, remove, updateTitle, updateColumn} from './database/modules/board/queries'
 import Board from './models/board';
 import Database from './database/database';
 window.board = new Board("This is a test title");
 window.boardInsert = insert;
 window.boardGetAll = getAll;
+window.boardRemove = remove;
+window.boardUpdateTitle = updateTitle;
+window.boardupdateColumn = updateColumn;
+window.boardupdateColumn = updateColumn;
 window.db = new Database();
-debugger
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
