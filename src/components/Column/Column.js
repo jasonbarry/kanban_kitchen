ce4b73d14ef5a5340d3c7a5c2af2a225c536c009
Isaac Owens <isaac.owens832@gmail.com>
2020-08-29 17:55:00 -0400

    Resolve merge conflicts

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import Card from '../Card/Card';
-import './Column.css';
-
-class Column extends React.Component {
-  render() {
-    return(
-      <div className="Column">
-        <Card />
-        <Card />
-        <Card />
-      </div>
-    )
-  }
-}
-
-export default Column
-=======
 import React from 'react';
 import Card from '../Card/Card';
 import './Column.css';
@@ -78,4 +59,3 @@ class Column extends React.Component {
 }
 
 export default DropTarget(Types.CARD, {}, collect)(Column)
->>>>>>> master
