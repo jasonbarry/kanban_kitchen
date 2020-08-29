b1ee9223550db9ca69b7ee8ad4989b2b1dff6356
Isaac Owens <isaac.owens832@gmail.com>
2020-08-29 17:56:06 -0400

    Build files

---
+++ build/precache-manifest.bc822689088bc8219703eeb08d55cddd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f481e7fbaa2a8c1bc8236f85adc8c2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d8d0653c4e09c06e8fd5",
+    "url": "./static/css/main.878d96c0.chunk.css"
+  },
+  {
+    "revision": "2e396b5a1cde7a16b5df",
+    "url": "./static/js/2.f34a665e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.f34a665e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d0653c4e09c06e8fd5",
+    "url": "./static/js/main.292aafed.chunk.js"
+  },
+  {
+    "revision": "54f86f9254d112b2695e",
+    "url": "./static/js/runtime-main.3668ed30.js"
+  }
+]);
